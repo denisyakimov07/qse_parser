080680f0a63784af54975c028739b7d4311f32fd
denisyakimov07 <59424027+denisyakimov07@users.noreply.github.com>
2021-12-01 01:05:05 -0500

    init

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ def creat_chrome():
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--no-sandbox")
-    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
     return driver
 
 
